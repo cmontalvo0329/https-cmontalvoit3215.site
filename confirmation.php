4a032c5fc5086d3694a50a5d1acf44b33c1594b2
cmontalvo0329 <73144619+cmontalvo0329@users.noreply.github.com>
2022-03-16 19:47:58 -0400

    Add files via upload

--- confirmation.php
+++ confirmation.php
@@ -1,4 +1,7 @@
 <!-- confirmation.php will display the below message after successful validation -->
+
 <?php
-echo "You form is successfully submitted.\nThank You for registeration.";
+
+echo "Your form is successfully submitted.\nThank You for registeration.";
+
 ?>
